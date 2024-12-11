9a46d7c0b4f1e9e1caea4572324ddf93a93a6159
Meybili <meybili200219@gmail.com>
2024-12-11 14:34:16 -0500

    feat: :fire: Update

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const swaggerDocument = require('./swagger');
 const app = express();
 const port = 3000;
 
-// Ruta al archivo WSDL físico
+
 const wsdlPath = './hello_world.wsdl';
 
-// Servicio SOAP que implementa la operación sayHello
+
 const service = {
   HelloWorldService: {
     HelloWorldPort: {
@@ -22,7 +22,6 @@ const service = {
   }
 };
 
-// Servir el WSDL físico en la ruta /wsdl
 app.get('/wsdl', (req, res) => {
   fs.readFile(wsdlPath, 'utf8', (err, data) => {
     if (err) {
@@ -34,17 +33,14 @@ app.get('/wsdl', (req, res) => {
   });
 });
 
-// Crear el servicio SOAP
 soap.listen(app, '/soap', service, wsdlPath);
 
-// Documentación de Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello world from JavaScript with  architecture style SOAP with documentation Swagger <3');
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor SOAP escuchando en http://localhost:${port}`);
 });
